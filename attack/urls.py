238e742fb85adcd2ab0558bbc0928264945a2421
Sam Val <samvalentine2811@gmail.com>
2021-07-05 12:46:56 +0700

    update login view to redirect if logged din

--- attack/urls.py
+++ attack/urls.py
@@ -5,7 +5,7 @@ from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-    path('login/', views.MyLogin.as_view(), name="login"),
+    path('login/', views.MyLogin.as_view(redirect_authenticated_user=True), name="login"),
     path('', views.account, name="account"),
     path('regenerate/', views.regenerate_table, name="regenerate"),
     path('withdrew/', views.withdrew, name='withdrew'),
