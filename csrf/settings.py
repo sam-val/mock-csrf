8676f7a06f85132e0714d0b7c34668205419461c
Sam Val <samvalentine2811@gmail.com>
2021-07-06 00:09:41 +0700

    same as previous

--- csrf/settings.py
+++ csrf/settings.py
@@ -18,8 +18,8 @@ import django_heroku
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
+ Quick-start development settings - unsuitable for production
+ See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
